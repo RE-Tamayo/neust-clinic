23944c86194eb117a933ccfc556ddbf2f2e19e29
Xnnovt <67304955+Xnnovt@users.noreply.github.com>
2022-05-19 02:31:27 +0800

    Created a registration modal
    created a registration modal using Javascript and CSS.

--- index.php
+++ index.php
@@ -5,29 +5,67 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
 	<link rel="stylesheet" href="library/css/student-login.css">
+  <link rel="stylesheet" href="library/css/register-modal.css">
+  <script defer src="library/js/register-modal.js"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body style="background-color: #1A1F7C;">
 	<div class="container">
       <div class="row login-container">
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 logo-container text-center">
-          <img src="library/assets/neust_logo.png" alt="logo" class="logo mt-5">
+          <img src="library/assets/neust_logo_animated.gif" alt="logo" class="logo mt-5">
           <h5 class="mt-3">Welcome to Neust Clinic</h5>
           <a href="#student">Not a student?<br>Click here.</a>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 form-container text-center">
           <h1 class="login-header">Log-In</h1>
           <hr>
-          <div class="d-flex justify-content-center">
+          <div class="d-flex justify-content-center mb-3">
             <form class="login-form">
-              <input type="text" name="username" placeholder="Username." class="form-control field">
-              <input type="password" name="password" placeholder="Password." class="form-control field spacer">
-              <button type="submit" class="m-2 login-button">Log-In</button>
+              <input type="text" name="username" placeholder="Username." class="form-control field mb-5">
+              <input type="password" name="password" placeholder="Password." class="form-control field mb-5">
+              <button type="submit" class="m-2 mt-3 login-button">Log-In</button>
             </form>
           </div>
-          <a href="#">Create Account</a>
+          <a class="btn btn-primary" id="myBtn" href="#">Register</a>
+          <a class="btn btn-primary" id="myBtn" href="#">Forgot Password?</a>
         </div>
       </div>
+
+      <div id="myModal" class="modal">
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <form action="" class="modal-form">
+            <center class="grid-col-span-7 mb-3"><label class="h2">Personal Information</label>
+              <hr></center>
+
+            <input type="file" class="form-control grid-col-span-7">
+            <input type="text" class="form-control grid-col-span-2" name="firstname" placeholder="First Name">
+            <input type="text" class="form-control grid-col-span-2" name="middlename" placeholder="Middle Name">
+            <input type="text" class="form-control grid-col-span-2" name="lastname" placeholder="Last Name">
+            <input type="text" class="form-control grid-col-span-1" name="Ext." placeholder="Ext.">
+            <input type="text" class="form-control grid-col-span-1" name="gender" placeholder="Gender">
+            <input type="date" class="form-control grid-col-span-2" name="birthday">
+            <input type="text" class="form-control grid-col-span-1" name="school-year" placeholder="S.Y.">
+            <input type="text" class="form-control grid-col-span-3" name="course" placeholder="Course">
+            <input type="text" class="form-control grid-col-span-7" name="guardian-contact" placeholder="Guardian Contact">
+
+            <center class="grid-col-span-7 mt-3"><label class="h2">Address</label>
+              <hr></center>
+
+            <input type="text" class="form-control">
+            <input type="text" class="form-control">
+            <input type="text" class="form-control">
+            <input type="text" class="form-control">
+            <input type="text" class="form-control">
+            <input type="text" class="form-control">
+            <input type="text" class="form-control">
+            <input type="text" class="form-control">
+            <button class="btn">Register</button>
+          </form>
+        </div>
+      </div>
+
     </div>
 </body>
 </html>
--- pages/student-dashboard.php
+++ pages/student-dashboard.php
@@ -7,29 +7,35 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../library/css/student-dashboard.css">
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-
     <title>Student Dashboard</title>
   </head>
   <body>
   	<div class="header">STUDENT DASHBOARD</div>
     <div class="grid-container">
+
       <div class="user-info">Picture
         <img src="#" alt="Profile Picture">
       </div>
+
       <div class="grid-col-span-2 user-info">
-        <h6>Personal Information</h6></div>
+        <h6>Personal Information</h6>
+      </div>
 
       <div class="user-actions">
         <img class="logo-size" src="../library/assets/sched.svg">
-        <button type="button" class="btn btn-warning">Schedule Visit</button>
+        <button type="button" class="btn button-action">Schedule Visit</button>
       </div>
+
       <div class="user-actions">
         <img class="logo-size" src="../library/assets/medcert.svg"> 
-        <button type="button" class="btn btn-warning">Request Medical </button></div>
+        <button type="button" class="btn button-action">Request Medical </button>
+      </div>
 
       <div class="user-actions">
         <img class="logo-size" src="../library/assets/view.svg"> 
-        <button type="button" class="btn btn-warning">View Profile</button></div>
+        <button type="button" class="btn button-action">View Profile</button>
+      </div>
+
     </div>
   </body>
 </html>
