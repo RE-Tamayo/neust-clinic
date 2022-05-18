17b913146f3ed5a52bd5bee40803ec5ba6bc5f07
Xnnovt <67304955+Xnnovt@users.noreply.github.com>
2022-05-18 18:32:11 +0800

    Create index
    Created folder structure ad index file.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+	<link rel="stylesheet" href="library/css/login.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div>Hello1</div>
+		<div>Hello2</div>
+	</div>
+</body>
+</html>
