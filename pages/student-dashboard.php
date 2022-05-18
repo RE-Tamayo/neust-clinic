a8304324e6f3fe1ad882864f0ac0fcd82d6804a7
Xnnovt <67304955+Xnnovt@users.noreply.github.com>
2022-05-18 21:32:00 +0800

    Edited student-dashboard

--- pages/student-dashboard.php
+++ pages/student-dashboard.php
@@ -1,46 +1,22 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../library/css/student-dashboard.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <title>Student Dashboard</title>
   </head>
   <body>
   	<div class="header">STUDENT DASHBOARD</div>
-  
-
-<div class="container-md"> 
-<div class="wrapper">
-<div>
-<h6>Profile Picture</h6>
-
-</div>
-<div class="left">
-	<div>
-<h6>Personal Info</h6>	
-<label>Name</label>
-<input type="text" name=""><br>	
-<label>Name</label>
-<input type="text" name="">
-</div>
-<div>
-<label>Name</label>
-<input type="text" name=""><br>	
-<label>Name</label>
-<input type="text" name="">
-</div>
-</div>	
-
-
-
-
-</div>
-</div>
-</body>
-
+    <div class="grid-container">
+      <div class="user-info">1</div>
+      <div class="grid-col-span-2 user-info">2</div>
+      <div class="user-actions">3</div>
+      <div class="user-actions">4</div>
+      <div class="user-actions">5</div>
+    </div>
+  </body>
 </html>
