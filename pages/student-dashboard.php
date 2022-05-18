c1ca298f3ce128f16391c256bee63617b38e6a6a
Jemxxxx <105546355+Jemxxxx@users.noreply.github.com>
2022-05-18 20:42:59 +0800

    hayup

---
+++ pages/student-dashboard.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../library/css/student-dashboard.css">
+    <title>Student Dashboard</title>
+  </head>
+  <body>
+  	<div class="header">STUDENT DASHBOARD</div>
+  
+
+<div class="container-md"> 
+<div class="wrapper">
+<div>
+<h6>Profile Picture</h6>
+
+</div>
+<div class="left">
+	<div>
+<h6>Personal Info</h6>	
+<label>Name</label>
+<input type="text" name=""><br>	
+<label>Name</label>
+<input type="text" name="">
+</div>
+<div>
+<label>Name</label>
+<input type="text" name=""><br>	
+<label>Name</label>
+<input type="text" name="">
+</div>
+</div>	
+
+
+
+
+</div>
+</div>
+</body>
+
+</html>
