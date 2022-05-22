443d5fbb194c229c231b1ec701b256ab74db1ad7
Jo <67304955+RE-Tamayo@users.noreply.github.com>
2022-05-22 21:35:03 +0800

    Recreating registration modal

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
 	<link rel="stylesheet" href="library/css/student-login.css">
-  <link rel="stylesheet" href="library/css/register-modal.css">
-  <script defer src="library/js/register-modal.js"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body style="background-color: #1A1F7C;">
@@ -21,63 +19,17 @@
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 form-container text-center">
           <h1 class="login-header">Log-In</h1>
           <hr>
-          <div class="d-flex justify-content-center mb-3">
-            <form class="login-form">
-              <input type="text" name="username" placeholder="Username." class="form-control field mb-5">
-              <input type="password" name="password" placeholder="Password." class="form-control field mb-5">
-              <button type="submit" class="m-2 mt-3 login-button">Log-In</button>
-            </form>
-          </div>
-          <a class="m-2" id="register" href="#">Register</a>
-          <a class="m-2" id="password-reset" href="#">Forgot Password?</a>
-        </div>
-      </div>
-
-      <div id="register-modal" class="modal">
-        <div class="modal-content">
-          <span class="close">&times;</span>
-          <form action="" class="modal-form">
-            <center class="grid-col-span-7 mb-3"><label class="h2">Personal Information</label>
-              <hr></center>
-
-            <input type="file" class="form-control grid-col-span-7">
-            <input type="text" class="form-control grid-col-span-2" name="firstname" placeholder="First Name">
-            <input type="text" class="form-control grid-col-span-2" name="middlename" placeholder="Middle Name">
-            <input type="text" class="form-control grid-col-span-2" name="lastname" placeholder="Last Name">
-            <input type="text" class="form-control grid-col-span-1" name="Ext." placeholder="Ext.">
-            <input type="text" class="form-control grid-col-span-1" name="gender" placeholder="Gender">
-            <input type="date" class="form-control grid-col-span-2" name="birthday">
-            <input type="text" class="form-control grid-col-span-1" name="school-year" placeholder="S.Y.">
-            <input type="text" class="form-control grid-col-span-3" name="course" placeholder="Course">
-            <input type="text" class="form-control grid-col-span-7" name="guardian-contact" placeholder="Guardian Contact">
-
-            <center class="grid-col-span-7 mt-3"><label class="h2">Address</label>
-              <hr></center>
-
-            <input type="text" class="form-control grid-col-span-1" name="house-number" placeholder="House Number">
-            <input type="text" class="form-control grid-col-span-3" name="street" placeholder="Street">
-            <input type="text" class="form-control grid-col-span-3" name="barangay" placeholder="Barangay">
-            <input type="text" class="form-control grid-col-span-3" name="city" placeholder="City">
-            <input type="text" class="form-control grid-col-span-4" name="province" placeholder="Province">
-
-            <center class="grid-col-span-7 mt-3"><label class="h2">Family</label>
-              <hr></center>
-
-            <input type="text" class="form-control grid-col-span-2" name="mother-firstname" placeholder="Mother's First Name">
-            <input type="text" class="form-control grid-col-span-2" name="mother-middlename" placeholder="Mother's Middle Name">
-            <input type="text" class="form-control grid-col-span-2" name="mother-lastname" placeholder="Mother's Last Name">
-            <input type="text" class="form-control grid-col-span-1" name="mother-ext" placeholder="Ext.">
-            <input type="text" class="form-control grid-col-span-7" name="mother-occupation" placeholder="Mother's Occupation">
-            <input type="text" class="form-control grid-col-span-2" name="father-firstname" placeholder="Father's First Name">
-            <input type="text" class="form-control grid-col-span-2" name="father-middlename" placeholder="Father's Middle Name">
-            <input type="text" class="form-control grid-col-span-2" name="father-lastname" placeholder="Father's Last Name">
-            <input type="text" class="form-control grid-col-span-1" name="father-ext" placeholder="Ext.">
-            <input type="text" class="form-control grid-col-span-7" name="father-occupation" placeholder="Father's Occupation">
-            <button class="btn btn-success grid-col-span-7">Register</button>
+        <div class="d-flex justify-content-center mb-3">
+          <form class="login-form">
+            <input type="text" name="username" placeholder="Username." class="form-control field mb-5">
+            <input type="password" name="password" placeholder="Password." class="form-control field mb-5">
+            <button type="submit" class="m-2 mt-3 login-button">Log-In</button>
           </form>
         </div>
+        <a class="m-2" id="register" href="#">Register</a>
+        <a class="m-2" id="password-reset" href="#">Forgot Password?</a>
       </div>
-
     </div>
+  </div>
 </body>
 </html>
